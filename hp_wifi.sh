9cc527e2b4f5204f915c2b29a72d5eac9c7b0fd2
DarkDante03 <49958605+DarkDante03@users.noreply.github.com>
2019-04-25 00:02:49 +0530

    Create hp_wifi.sh

---
+++ hp_wifi.sh
@@ -0,0 +1,13 @@
+
+cd Downloads
+git clone -b extended --single-branch https://github.com/lwfinger/rtlwifi_new.git
+cd rtlwifi_new
+make
+sudo make install
+sudo modprobe rtl8723de
+
+
+Respect Developer 
+
+Script By Piyush Awchar
+
